db6e66957fa280a2f723ccbba9c376f1b9feee31
Shelby Shum <sshum00@gmail.com>
2023-04-13 19:35:06 -0700

    Updates

---
+++ precache-manifest.6cdf115cca62cdb4fff60474125cac9b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b0f4e228b9e75d828bb0",
+    "url": "/espresso/static/css/main.cfe110b3.chunk.css"
+  },
+  {
+    "revision": "b0f4e228b9e75d828bb0",
+    "url": "/espresso/static/js/main.2f484ad6.chunk.js"
+  },
+  {
+    "revision": "6e7f3531822802bc30e8",
+    "url": "/espresso/static/js/runtime~main.764601fd.js"
+  },
+  {
+    "revision": "2e7eda43754939755797",
+    "url": "/espresso/static/js/2.e472d5c9.chunk.js"
+  },
+  {
+    "revision": "eeb29d83d1bcbaba4e91",
+    "url": "/espresso/static/js/3.82db40f9.chunk.js"
+  },
+  {
+    "revision": "4ccb38366e404480bfe3ed10d9cf26f8",
+    "url": "/espresso/static/media/nobubar.4ccb3836.png"
+  },
+  {
+    "revision": "50f9cc148a1819bfbc57449b2b7f7680",
+    "url": "/espresso/index.html"
+  }
+];
